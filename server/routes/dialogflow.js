80b8108151c4c341a2d5472b479229c71f2fdc69
--list <nowakdariusz03@gmail.com>
2021-11-11 03:10:10 +0100

    Connecting backend to dialogflow api

---
+++ server/routes/dialogflow.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const dialogflow = require("dialogflow");
+const uuid = require("uuid");
+const config = require("../config/keys");
+const structjson = require("./structjson.js");
+
+const router = express.Router();
+
+const projectId = config.googleProjectID;
+const sessionId = config.dialogFlowSessionID;
+const languageCode = config.dialogFlowSessionLanguageCode;
+
+// Create new session
+const sessionClient = new dialogflow.SessionsClient();
+const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+
+// Text Query Route
+router.post("/textQuery", async (req, res) => {
+  //We need to send some information that comes from the client to Dialogflow API
+  // The text query request.
+
+  // The text query request.
+  const request = {
+    session: sessionPath,
+    queryInput: {
+      text: {
+        // req.body.text to bodyparser - przechwytuje cialo http requesta (body w postmanie)
+        //   text: req.body.text,
+        text: req.body.text,
+        // The language used by the client (en-US)
+        languageCode: languageCode,
+      },
+    },
+  };
+
+  // Send request and log result
+  const responses = await sessionClient.detectIntent(request); //tutaj jest nasz response od api dialogflow
+  console.log("Detected intent");
+  const result = responses[0].queryResult;
+  console.log(`  Query: ${result.queryText}`);
+  console.log(`  Response: ${result.fulfillmentText}`);
+  //wysylanie responsa do frontendu
+  res.send(result);
+});
+
+module.exports = router;
---
+++ server/server.js
@@ -0,0 +1,20 @@
+// importing
+const express = require("express");
+const dialogflowRoutes = require("./routes/dialogflow.js");
+const bodyParser = require("body-parser");
+
+// app config
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/dialogflow", dialogflowRoutes);
+// DB config
+
+// api routes
+
+// listen
+app.listen(port, () => console.log(`Server running at localhost: ${port}`));

f0b5816fe4060d8db9a3caeebf9462365822fa0d
Dariusz Nowak <dariusz.nowak012@gmail.com>
2021-12-12 01:20:54 +0100

    added welcome with name

--- server/routes/fulfillmentRoutes.js
+++ server/routes/fulfillmentRoutes.js
@@ -7,25 +7,38 @@ const { WebhookClient } = require("dialogflow-fulfillment");
 // const bcrypt = require("bcrypt");
 // const jwt = require("jsonwebtoken");
 const router = express.Router();
-// const User = require("../models/User");
+const User = require("../models/User");
 
 // const secret = "secret123";
 
 router.post("/fulfillment", async (req, res) => {
   const agent = new WebhookClient({ request: req, response: res });
 
-  function welcome(agent) {
-    agent.add("Welcome to my fulfillment");
-  }
+  console.log("######################fulfillment##########################");
+  console.log(req.body);
 
   const fallback = (agent) => {
     agent.add("I didn't understand");
     agent.add("I'm sorry, can you try again?");
   };
   let intentMap = new Map(); //to intents map we have intents with fulfillment enabled
-  intentMap.set("order food", welcome);
 
   intentMap.set("Default Fallback Intent", fallback);
+
+  //test//powitanie usera po imieniu po zalogowaniu
+  intentMap.set("Welcome", async (agent) => {
+    const userId = req.body.session.split("bot-session")[1];
+
+    const user = await User.findOne({ _id: userId });
+    const fullName = user.fullName.split(" ")[0];
+
+    agent.add(`Hello ${fullName}! How can I help you?`);
+  });
+
+  intentMap.set("order food", (agent) => {
+    agent.add(req.body.queryResult.fulfillmentText);
+    //tutaj powinno mi wyslac liste kategorii do frontendu
+  });
 
   agent.handleRequest(intentMap);
 });
